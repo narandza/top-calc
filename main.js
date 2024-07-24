afe5cfa3d5fe9c97663a4d06b16feb364b0ae97d
dimitrije <dimitrijejovanovic90210@gmail.com>
2024-07-24 19:54:21 +0200

    redesing styling, deleted logic

--- main.js
+++ main.js
@@ -1,106 +1,99 @@
-// Operations
-
-const add = (a, b) => {
-  return a + b;
-};
-
-const subtract = (a, b) => {
-  return a - b;
-};
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-const divide = (a, b) => {
-  return a / b;
-};
-
-const operation = (a, b, operation) => {
-  operation(a, b);
-};
-
-const clear = () => {
-  //todo
-};
-
-const negate = () => {
-  //TODO
-};
-
-// Display
-
-const resultDisplay = document.querySelector(".result-display");
-const operationDisplay = document.querySelector(".operation-display");
-let result = "";
-let equation = "";
-
-const enterNumber = (num) => {
-  if (!result) {
-    resultDisplay.textContent += num;
-  }
-};
-
-// DOM
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, ",", 0, "="];
-
-const operatingFunctions = [
+const buttons = [
   {
-    name: "negate",
-    function: negate,
-    symbol: "+/-",
-  },
-
-  {
-    name: "multiply",
-    function: multiply,
-    symbol: "*",
+    value: "AC",
+    type: "all-clear",
+    span2: true,
   },
   {
-    name: "divide",
-    function: divide,
-    symbol: "/",
+    value: "DEL",
+    type: "delete",
   },
   {
-    name: "clear",
-    function: clear,
-    symbol: "C",
+    value: "/",
+    type: "operation",
   },
   {
-    name: "add",
-    function: add,
-    symbol: "+",
+    value: "1",
+    type: "number",
   },
   {
-    name: "subtract",
-    function: subtract,
-    symbol: "-",
+    value: "2",
+    type: "number",
+  },
+  {
+    value: "3",
+    type: "number",
+  },
+  {
+    value: "*",
+    type: "operation",
+  },
+  {
+    value: "4",
+    type: "number",
+  },
+  {
+    value: "5",
+    type: "number",
+  },
+  {
+    value: "6",
+    type: "number",
+  },
+  {
+    value: "+",
+    type: "operation",
+  },
+  {
+    value: "7",
+    type: "number",
+  },
+  {
+    value: "8",
+    type: "number",
+  },
+  {
+    value: "9",
+    type: "number",
+  },
+  {
+    value: "-",
+    type: "operation",
+  },
+  {
+    value: ".",
+    type: "number",
+  },
+  {
+    value: "0",
+    type: "number",
+  },
+  {
+    value: "=",
+    type: "equals",
+    span2: true,
   },
 ];
 
-const calcBodyNumbers = document.querySelector(".buttons-container");
+const calc = document.querySelector(".calculator");
 
-operatingFunctions.forEach((operation) => {
-  const operationButton = document.createElement("div");
-  operationButton.classList.add("button");
-  operationButton.classList.add("opBtn");
-  operationButton.textContent = operation.symbol;
-
-  operationButton.addEventListener("click", () => {
-    console.log("clicked");
-  });
-  calcBodyNumbers.appendChild(operationButton);
+buttons.forEach((button) => {
+  const btn = document.createElement("button");
+  btn.textContent = button.value;
+  btn.setAttribute(`data-${button.type}`, "");
+  btn.classList.add("button");
+  if (button.span2) {
+    btn.classList.add("span-two");
+  }
+  calc.appendChild(btn);
 });
 
-numbers.forEach((number) => {
-  const numberButton = document.createElement("div");
-  numberButton.classList.add("button");
-  numberButton.classList.add("num");
-  number === "=" && numberButton.classList.add("primary");
-  numberButton.textContent = number;
-  numberButton.addEventListener("click", (e) => {
-    enterNumber(e.target.textContent);
-  });
-  calcBodyNumbers.appendChild(numberButton);
-});
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationNumbers = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandElement = document.querySelector("[data-current-operand]");
